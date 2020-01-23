3c9825adfcd16a60f8b1531f666cd6440b7f0c2f
Code <code@COCTL-41270.local>
2020-01-23 15:48:45 -0800

    last comit of milestone#2

--- WebStore/src/app/app-routing.module.ts
+++ WebStore/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './views/pages/landing/landing.component';
+import { ProductsComponent } from './views/pages/products/products.component';
+import { CartComponent } from './views/pages/cart/cart.component';
+import { LogInComponent } from './views/pages/log-in/log-in.component';
+import { ContactComponent } from './views/pages/contact/contact.component';
 
 
 const routes: Routes = [
   {path: 'home', component: LandingComponent },
+  {path: 'products', component: ProductsComponent },
+  {path: 'cart', component: CartComponent },
+  {path: 'LogIn', component: LogInComponent },
+  {path: 'contact', component: ContactComponent },
   {path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
