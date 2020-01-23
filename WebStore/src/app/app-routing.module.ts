21d14a79a48c0d49806aa18aad4355af0f02dc87
Code <code@COCTL-41270.local>
2020-01-23 15:03:58 -0800

    overlay and starting routing

--- WebStore/src/app/app-routing.module.ts
+++ WebStore/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './views/pages/landing/landing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: LandingComponent },
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
