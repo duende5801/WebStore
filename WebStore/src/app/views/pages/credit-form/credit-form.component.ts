9d3c6ded03b191c0731eabfb445a7815b2b788af
David Abdallah <davidabdallah@Davids-MacBook-Pro.local>
2020-04-15 14:43:50 -0700

    full stripe lecture vid

--- WebStore/src/app/views/pages/credit-form/credit-form.component.ts
+++ WebStore/src/app/views/pages/credit-form/credit-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {loadStripe} from '@stripe/stripe-js';
-import { NgForm } from '@angular/forms';
+import { OrderRequest } from 'src/app/shared/interfaces/order';
+import { ApiServiceService } from 'src/app/shared/api-service.service';
 
 @Component({
   selector: 'app-credit-form',
@@ -8,11 +8,38 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./credit-form.component.scss']
 })
 export class CreditFormComponent implements OnInit {
+  public sessionId: string;
+  public orderRequest: OrderRequest[] = [];
 
-  constructor() { }
-
+  constructor(private service: ApiServiceService) { }
 
   ngOnInit() {
+    this.orderRequest.push(
+    {
+      InventoryItemId: 1,
+      Quantity: 3
+    },
+    {
+      InventoryItemId: 2,
+      Quantity: 1,
+    },
+    {
+      InventoryItemId: 3,
+      Quantity: 2
+    });
   }
-
+  submit() {
+    this.service.initSession(this.orderRequest)
+      .subscribe(
+        (resp: any)=> this.redirect(resp.id),
+      );
+  }
+  redirect(id: number) {
+    stripe.redirectToCheckout({
+      sessionId: id
+    }).then(function(result: { error: { message: any; }; })
+    {
+      window.alert(result.error.message);
+    });
+  }
 }
--- WebStore/src/app/views/views.module.ts
+++ WebStore/src/app/views/views.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarModule } from 'primeng/sidebar';
 import { PasswordModule } from 'primeng/password';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -10,6 +11,7 @@ import { PasswordModule } from 'primeng/password';
     CommonModule,
     SidebarModule,
     PasswordModule,
+    SharedModule
   ]
 })
 export class ViewsModule { }
