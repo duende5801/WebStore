cd9732ee0962f90a3e850abaf2f48965c7e3fb8f
David Abdallah <davidabdallah@Davids-MacBook-Pro.local>
2020-05-30 17:04:14 -0700

    production env

--- WebStore/src/environments/environment.prod.ts
+++ WebStore/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // api: 'http://localhost:5000',
-  // stripeKey: 'pk_test_jagbIMSMwI2VF7cFq1o1XOtP00p32PBVwm'
+  api: 'http://davidabdallah.azurewebsites.net/Projects/WebStore',
+  stripeKey: 'pk_test_jagbIMSMwI2VF7cFq1o1XOtP00p32PBVwm'
 
 };
